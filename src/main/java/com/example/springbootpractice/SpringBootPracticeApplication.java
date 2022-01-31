1821aaf11604118d7a37c44ae6a76f324d950587
donghyeon0915 <dongdong0915_@naver.com>
2022-02-01 02:04:47 +0900

    JPA를 활용한 테스트 코드 추가(JPA 영속성 컨텍스트)

--- src/main/java/com/example/springbootpractice/SpringBootPracticeApplication.java
+++ src/main/java/com/example/springbootpractice/SpringBootPracticeApplication.java
@@ -1,13 +1,30 @@
 package com.example.springbootpractice;
 
+import com.example.springbootpractice.entity.Article;
+import com.example.springbootpractice.entity.Comment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringBootPracticeApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(SpringBootPracticeApplication.class, args);
+
+/*        Comment comment = Comment.builder()
+                .id(1L)
+                .nickname("Park")
+                .body("굳 윌 헌팅")
+                .article(new Article(4L, "당신의 인생 영화는 ?", "댓글 ㄱ"))
+                .build();
+
+        log.info("아티클 가져오기 {}", comment.getArticle().toString());*/
+
     }
 
 }
--- src/test/java/com/example/springbootpractice/repository/CommentRepositoryTest.java
+++ src/test/java/com/example/springbootpractice/repository/CommentRepositoryTest.java
@@ -2,23 +2,25 @@ package com.example.springbootpractice.repository;
 
 import com.example.springbootpractice.entity.Article;
 import com.example.springbootpractice.entity.Comment;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 @DataJpaTest // JPA와 연동한 테스트임을 지정
 class CommentRepositoryTest {
 
     @Autowired
     private CommentRepository commentRepository;
+
+    @Autowired
+    private ArticleRepository articleRepository;
     
     @Test
     @DisplayName("특정 게시글의 모든 댓글 조회") //테스트 결과에 보여줄 이름
@@ -277,5 +279,27 @@ class CommentRepositoryTest {
             // 검증
             assertEquals(expected.toString(), comments.toString());
         }
+    }
+
+    @Test
+    void JPA를_활용한_댓글과_아티클_조회() {
+        // 준비
+        System.out.println("------------------------- 댓글 조회 -------------------------------");
+        Comment comment = commentRepository.findById(1L).orElse(null); 
+        // article_id를 기준으로 left outer join이 됨
+        System.out.println(comment);
+        
+        // 수행
+        System.out.println("------------------------- 댓글에서 게시글 가져오기 -------------------------------");
+        Article article = comment.getArticle();
+        System.out.println(article);
+
+        // 예상
+        System.out.println("------------------------- 게시글 조회 -------------------------------");
+        Article expected = articleRepository.findById(4L).orElse(null);
+        //articleRepository의 쿼리는 안나오는 이유 ?
+        
+        // 검증
+        assertEquals(expected.toString(), article.toString());
     }
 }
